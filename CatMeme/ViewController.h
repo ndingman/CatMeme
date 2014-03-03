538e4c0a31580ceec21498bf08609f826268c909
Neil Dingman <neil.dingman@mentormate.com>
2014-03-03 10:11:32 -0600

    Initial Commit

---
+++ CatMeme/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CatMeme
+//
+//  Created by NEIL DINGMAN on 3/3/14.
+//  Copyright (c) 2014 Neil Dingman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CatMemeTests/CatMemeTests.m
@@ -0,0 +1,34 @@
+//
+//  CatMemeTests.m
+//  CatMemeTests
+//
+//  Created by NEIL DINGMAN on 3/3/14.
+//  Copyright (c) 2014 Neil Dingman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CatMemeTests : XCTestCase
+
+@end
+
+@implementation CatMemeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
